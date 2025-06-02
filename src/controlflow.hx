0f989969067082e1c0b3d43fac23aeaae397cf00
Ymisty <yordidevogel9@outlook.com>
2025-06-02 12:27:15 +0200

    update

---
+++ src/controlflow.hx
@@ -0,0 +1,12 @@
+// Control flow If / Else.
+
+class Main {
+    static function main() {
+        var age = 20;
+        if (age >= 20) {
+            trace ("old");
+        } else {
+            trace ("young"); 
+        }
+    }
+}
